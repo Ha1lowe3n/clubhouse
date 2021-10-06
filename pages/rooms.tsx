83988ee246f30b69be8c319347038834dce12482
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-10-06 15:27:21 +0800

    add ConversationCard (room card in All conversations)

--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -1,11 +1,35 @@
+import { Button } from "../components/Button";
+import { ConversationCard } from "../components/ConversationCard";
 import { Header } from "../components/Header";
 
 export default function RoomsPage() {
+    const guests = [
+        "Sasha Beliy",
+        "Ilyuha CHAOSHORSE",
+        "TATSTSSSSSSSSSSSSSSSSSSSS",
+    ];
+
     return (
         <>
             <Header />
             <div className="container">
-                <h1>ТУТ БУДУТ ВСЕ СТРАНИЦЫ</h1>
+                <div className="mt-40 d-flex align-items-center justify-content-between">
+                    <h1>All conversations</h1>
+                    <Button color="green">+ Start room</Button>
+                </div>
+
+                <div className="mt-20">
+                    <ConversationCard
+                        title="Create clubhouse-clone"
+                        avatars={[
+                            "https://i.pinimg.com/originals/3c/4b/fd/3c4bfd9273bfc4827e76709e3db4deec.jpg",
+                            "https://slovnet.ru/wp-content/uploads/2018/12/6-65.jpg",
+                        ]}
+                        guests={guests}
+                        guestsCount={guests.length}
+                        speakersCount={2}
+                    />
+                </div>
             </div>
         </>
     );
