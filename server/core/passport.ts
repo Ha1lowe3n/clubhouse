3205d5dcc2308e6fd6c3f45b7e266ee2dbe90da9
Ha1lowe3n <Ha1lowe3n@yandex.ru>
2021-10-11 22:48:29 +0800

    4: add migrations, create User in db

--- server/core/passport.ts
+++ server/core/passport.ts
@@ -1,6 +1,10 @@
 import passport from "passport";
 import { Strategy as GitHubStrategy } from "passport-github";
 
+import { User } from "../../models";
+
+console.log(User);
+
 passport.use(
     new GitHubStrategy(
         {
@@ -8,15 +12,27 @@ passport.use(
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
             callbackURL: `http://localhost:${process.env.PORT}/auth/github/callback`,
         },
-        (accessToken, refreshToken, profile, cb) => {
-            const user = {
+        async (accessToken, refreshToken, profile, cb) => {
+            const userData = {
                 fullname: profile.displayName,
                 avatarUrl: profile.photos?.[0].value,
+                isActive: 0,
+                username: profile.username,
+                phone: "",
             };
-            console.log(accessToken, refreshToken, profile, cb);
+            const user = await User.create(userData);
+            console.log(userData);
             cb();
         }
     )
 );
+
+// development: {
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+// },
 
 export { passport };
